bd46aa9ebfb5fe3c0485e8ef90493427d987ec4f
CorbitaKit <corbita.khatezz@gmail.com>
2025-01-13 20:02:05 +0800

    safe commit

--- app/Services/Auth/UserService.php
+++ app/Services/Auth/UserService.php
@@ -51,32 +51,6 @@ class UserService extends Service
                 ->where('is_sms_sent', 0)->get();
 
 
-                foreach ($overduePaymentPlans as $overDue) {
-                    $cleanedNumber = preg_replace('/[^0-9]/', '', $overDue->user->personal_info->phone_number);
-          
-                    $response = Http::withHeaders([
-                        'Authorization' => 'App 37e306dbf911a4db4de9da34a281639d-5bc56cee-cb75-4e21-8915-1d9f0b41d091', // Replace YOUR_API_KEY with your actual key
-                        'Content-Type' => 'application/json',
-                        'Accept' => 'application/json'
-                    ])->post('https://e1p4v1.api.infobip.com/sms/2/text/advanced', [
-                        'messages' => [
-                            [
-                                'destinations' => [['to' => $cleanedNumber]],
-                                'from' => '447491163443',
-                                'text' => 'Good day! this is from Aldebal Real State Services. we just want to inform you that you missed your payment for ' . $overDue->due_date
-                            ]
-                        ]
-                    ]);
-            
-                    if ($response->successful()) {
-                        $overDue->is_sms_sent = 1;
-                        $overDue->save();
-                    } else {
-                        dd($response->status());
-                    }
-                }
-
-                
             }
             $request->session()->regenerate();
             return back()->with(['message' => 'Authorized', 'user' => parent::doFindById(Auth::user()->id)]);
@@ -93,7 +67,7 @@ class UserService extends Service
 
             $convertedData = $data->all();
             if (!isset($convertedData['user_id'])) {
-             
+
                 $user = $this->doCreate($convertedData['account_info']);
                 $user->email_verified_at = now();
                 $user->save();
@@ -104,22 +78,22 @@ class UserService extends Service
                 if ($convertedData['file']) {
                     $fileUpload = $data['file']['value'];
                     $path = $this->uploadFile($fileUpload, 'user/requirements');
-    
+
                     $file = $this->fileService->doCreate([
                         'filename' => $fileUpload->getClientOriginalName(),
                         'url' => $path
                     ]);
-    
+
                     $user->files()->save($file);
                 }
             }
 
-            
+
             $convertedData['personal_info']['address'] = 'custom address';
             $this->employeeService->doCreate($convertedData['personal_info']);
             $this->addressSerivce->doCreate($convertedData['personal_address']);
             $this->workService->doCreate($convertedData['work_details']);
-            
+
         });
     }
 
